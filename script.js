728531a59a78241c9d42a76e56b6b5feb41c9c30
Pratikbhttr45 <78664780+Pratikbhttr45@users.noreply.github.com>
2023-02-01 12:47:10 +0545

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,23 @@
 const taskInput = document.querySelector("#taskInput");
+const taskDate = document.querySelector("#taskDate");
+const taskTime = document.querySelector("#taskTime");
 const addTaskBtn = document.querySelector("#addTaskBtn");
 const taskList = document.querySelector("#taskList");
 
 addTaskBtn.addEventListener("click", function() {
   const taskValue = taskInput.value;
+  const taskDateValue = taskDate.value;
+  const taskTimeValue = taskTime.value;
   if (!taskValue) return;
 
   const taskLi = document.createElement("li");
-  taskLi.innerHTML = taskValue;
+  taskLi.innerHTML = `${taskValue} - ${taskDateValue} ${taskTimeValue}`;
+  taskLi.addEventListener("click", function() {
+    taskLi.classList.toggle("completed");
+  });
   taskList.appendChild(taskLi);
 
   taskInput.value = "";
+  taskDate.value = "";
+  taskTime.value = "";
 });
